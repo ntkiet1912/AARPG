098a9162660eb0d9b83771c2aec7c1e4e32ce8a7
ntkiet1912 <pupgkiet@gmail.com>
2025-07-23 00:23:46 +0700

    Add player attack stage , audio , effect and player 's shadow

--- Player/Scripts/state_idle.gd
+++ Player/Scripts/state_idle.gd
@@ -1,6 +1,7 @@
 class_name State_Idle extends State
 
 @onready var walk: State_Walk = $"../Walk"
+@onready var attack: State_Attack = $"../Attack"
 
 ## What happen when the player enters this State?
 func Enter() ->void:
@@ -24,4 +25,7 @@ func Physics( _delta : float ) -> State:
 
 ## What happens with input events in this state?
 func HandleInput( _event : InputEvent) -> State:
+	
+	if _event.is_action_pressed("attack") : 
+		return attack
 	return null
--- Player/Scripts/state_walk.gd
+++ Player/Scripts/state_walk.gd
@@ -2,6 +2,7 @@ class_name State_Walk extends State
 
 @export var move_speed : float = 100.0
 @onready var idle: State_Idle = $"../Idle"
+@onready var attack: State_Attack = $"../Attack"
 
 
 
@@ -32,4 +33,6 @@ func Physics( _delta : float ) -> State:
 
 ## What happens with input events in this state?
 func HandleInput( _event : InputEvent) -> State:
+	if _event.is_action_pressed("attack") : 
+		return attack
 	return null
